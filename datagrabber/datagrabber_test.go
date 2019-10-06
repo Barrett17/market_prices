5f9962ec404135ff7adbaedc202a5c7209fb7db8
Dario Casalinuovo <b.vitruvio@gmail.com>
2019-10-06 20:23:40 +0200

    Add concurrency test

---
+++ datagrabber/datagrabber_test.go
@@ -0,0 +1,17 @@
+package datagrabber
+
+import (
+	"testing"
+
+	"github.com/Barrett17/market_prices/types"
+)
+
+// Warning don't run this test often as create a lot
+// of connections.
+func TestConcurrency(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		go GetData(types.EUR_USD)
+		go pollData()
+		go GetData(types.EUR_GBP)
+	}
+}
