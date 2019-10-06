1d287a400539960252dd0cf457ff07ef32391d94
Dario Casalinuovo <b.vitruvio@gmail.com>
2019-10-06 13:11:19 +0200

    datagrabber: Set up some synchronization skeleton

--- datagrabber/datagrabber.go
+++ datagrabber/datagrabber.go
@@ -1,12 +1,46 @@
 package datagrabber
 
 import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
 )
 
-func DataGrabberThread() {
-	
+var mutex = &sync.Mutex{}
+var wg sync.WaitGroup
+
+// Poll new data each 60 seconds
+const Quantum = 10
+const Timeout = time.Minute*2
+
+func init() {
+	go func() {
+		ticker := time.NewTicker(time.Second * Quantum)
+		defer ticker.Stop()
+
+		started := time.Now()
+		for {
+			pollData();
+
+			now := <-ticker.C
+			if now.Sub(started) > Timeout {
+				log.Printf("pollData() has timed out!");
+			}
+		}
+	}()
 }
 
-func GetLastData() {
+func pollData() {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	fmt.Println("pollData");
+}
+
+func GetData() {
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	
 }
