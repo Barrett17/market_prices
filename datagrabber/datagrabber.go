132e494ddad908451c2818b4419af2949b153b4a
Dario Casalinuovo <b.vitruvio@gmail.com>
2019-10-06 14:21:19 +0200

    datagrabber: Grab data from the remote server

--- datagrabber/datagrabber.go
+++ datagrabber/datagrabber.go
@@ -1,32 +1,46 @@
 package datagrabber
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	"log"
+	"net/http"
 	"sync"
 	"time"
 )
 
-var mutex = &sync.Mutex{}
-var wg sync.WaitGroup
-
 // Poll new data each 60 seconds
-const Quantum = 10
-const Timeout = time.Minute*2
+const Quantum = 60
+const Timeout = Quantum
+const RemoteUrl = "https://api.exchangeratesapi.io/"
+
+var mutex = &sync.Mutex{}
+var lastRates = ExchangeLatestResponse{}
+var lastWeekRate float64
+
+type Rates struct {
+	USD float64 `json:"USD"`
+	GBP float64 `json:"GBP"`
+}
+
+type ExchangeLatestResponse struct {
+	Rates Rates `json:"rates"`
+	Base string `json:"base"`
+	Date string `json:"date"`
+}
+
+type ExchangeHistoryResponse struct {
+	Rates map[string]Rates `json:"rates"`
+}
 
 func init() {
 	go func() {
 		ticker := time.NewTicker(time.Second * Quantum)
 		defer ticker.Stop()
 
-		started := time.Now()
 		for {
 			pollData();
-
-			now := <-ticker.C
-			if now.Sub(started) > Timeout {
-				log.Printf("pollData() has timed out!");
-			}
+			<-ticker.C
 		}
 	}()
 }
@@ -35,7 +49,79 @@ func pollData() {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	fmt.Println("pollData");
+	fmt.Println("Polling data");
+
+	err := pollCurrentRates();
+	if (err != nil) {
+		fmt.Println(err)
+	}
+
+	err = pollWeeklyRates();
+	if (err != nil) {
+		fmt.Println(err)
+	}
+}
+
+// Assumes the mutex is already locked when called
+func pollCurrentRates() error {
+	req, _ := http.NewRequest("GET", RemoteUrl+"latest?symbols=USD,GBP", nil)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		// TODO define errors
+		return errors.New("Wrong StatusCode from server")
+	}
+
+	response := ExchangeLatestResponse{}
+
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&response); err != nil {
+		return errors.New("Cannot decode data")
+	}
+
+	lastRates = response
+
+	s := fmt.Sprintf("%f %f", response.Rates.GBP, response.Rates.USD);
+	fmt.Println(s)
+
+	return nil
+}
+
+// Assumes the mutex is already locked when called
+func pollWeeklyRates() error {
+	req, _ := http.NewRequest("GET", RemoteUrl+"history?start_at=2018-01-01&end_at=2018-09-01&symbols=USD,GBP", nil)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New("Wrong StatusCode from server")
+	}
+
+	response := ExchangeHistoryResponse{}
+
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&response); err != nil {
+		return errors.New("Cannot decode data")
+	}
+
+	//lastWeekRates = response
+
+	//fmt.Println(response.Rates)
+
+	return nil
 }
 
 func GetData() {
