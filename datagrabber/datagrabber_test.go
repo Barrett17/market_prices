6fd2292afbdab0a52189ff92684f11b8e62b9308
Dario Casalinuovo <b.vitruvio@gmail.com>
2019-10-08 00:05:51 +0200

    Add tests

--- datagrabber/datagrabber_test.go
+++ datagrabber/datagrabber_test.go
@@ -9,7 +9,7 @@ import (
 // Warning don't run this test often as create a lot
 // of connections.
 func TestConcurrency(t *testing.T) {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 5; i++ {
 		go GetData(types.EUR_USD)
 		go pollData()
 		go GetData(types.EUR_GBP)
--- server_test.go
+++ server_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
+	"sync"
+	"time"
 	"testing"
 
 	"github.com/Barrett17/market_prices/types"
@@ -23,17 +25,57 @@ func TestMain(t *testing.T) {
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		t.Errorf("Cannot GET data!")
+		t.Errorf("Cannot GET data")
 	}
 
 	response := types.HTTPResponse{}
 
 	decoder := json.NewDecoder(res.Body)
 	if err := decoder.Decode(&response); err != nil {
-		t.Errorf("Cannot decode data!")
+		t.Errorf("Cannot decode data")
 	}
 
 	if strings.Compare(response.Ticker, "USD") != 0 {
-		t.Errorf("Response has wrong ticker!")
+		t.Errorf("Response has wrong ticker")
+	}
+}
+
+func TestMultipleRequests(t *testing.T) {
+	var wg sync.WaitGroup
+	count := 1000
+	wg.Add(count)
+
+	for i := 0; i < count; i++ {
+		go func(){
+			req, _ := http.NewRequest("GET", url+"GBP", nil)
+
+			client := &http.Client{}
+			res, err := client.Do(req)
+			if err != nil {
+				t.Errorf("Req Error: " + err.Error())
+			}
+
+			defer res.Body.Close()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func TestResponseTime(t *testing.T) {
+	req, _ := http.NewRequest("GET", url+"USD", nil)
+
+	client := &http.Client{}
+	start := time.Now()
+	res, err := client.Do(req)
+	end := time.Now()
+	if err != nil {
+		t.Errorf("Req Error: " + err.Error())
+	}
+
+	defer res.Body.Close()
+	elapsed := end.Sub(start)
+	if (time.Duration(elapsed)/time.Millisecond > 1) {
+		t.Errorf("Request over 1ms")
 	}
 }
